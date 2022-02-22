035ac46b933ade99a26678ff9202e614818498bf
Tanmay <tanmay44200@gmail.com>
2022-02-22 15:20:10 +0530

    Forms work!

--- myapp/views.py
+++ myapp/views.py
@@ -9,34 +9,18 @@ from .models import UIDS
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html', {'name': 'Tanmay'})
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+    
 
 def register(request):  
-    return render(request, 'register.html', {'name': 'Tanmay'})
+    return render(request, 'register.html')
 
 # def user(request):
 #     form = CustomerForm()
 #     return render(request, 'customer_form.html', {'form' : form})
     #fields = ('category', 'company_name', 'location', 'admin_name', 'username', 'password', 'cnf_password')
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = LoginForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/base/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = LoginForm()
-
-    return render(request, 'login.html', {'form': form})
-
+    
 
 def add(request):
     val1= int(request.POST["num1"])
